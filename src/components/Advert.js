33b37c726dd3a35d8222889bfef43c2681be79e7
Kelvin Stephano <kstephano@gmail.com>
2021-08-31 19:00:58 +0100

    created advert component

---
+++ src/components/Advert.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import InfoIcon from '@material-ui/icons/Info';
+
+import '../css/Advert.css'
+
+function Advert() {
+    return (
+        <div className="advert">
+            <div className="advert__image-container">
+                <img 
+                    src="https://m.media-amazon.com/images/I/511d3Tfh3ML.jpg" 
+                    alt="Advert" 
+                    className="advert__image" 
+                />
+            </div>  
+            <div className="advert__info-container">
+                <span>Sponsored</span>
+                <InfoIcon className="advert__info-logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Advert
